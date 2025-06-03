f3b49b938135f3d3065a42c8be0bc4a7dc2b2207
Marco <marcogoc951@gmail.com>
2025-06-03 17:03:22 -0400

    after  prettier write whihc added ugly semi-colons lol

--- frontend/app/.server/locales/index.ts
+++ frontend/app/.server/locales/index.ts
@@ -1,5 +1,6 @@
 import appEn from './app-en';
 import appFr from './app-fr';
+
 import gcwebEn from '~/.server/locales/gcweb-en.json';
 import gcwebFr from '~/.server/locales/gcweb-fr.json';
 
@@ -18,87 +19,86 @@ export const i18nResources = {
   fr: i18nResourcesFr,
 } as const satisfies Record<Language, typeof i18nResourcesEn>;
 
-
 export type I18nResources = typeof i18nResources;
 
 export interface Translations {
   index: {
-    about: string
-    dashboard: string
-    pageTitle: string
-    navigate: string
-    register: string
-  },
+    about: string;
+    dashboard: string;
+    pageTitle: string;
+    navigate: string;
+    register: string;
+  };
   register: {
-    employee: string
-    hiringManager: string
-    pageTitle: string
-  },
+    employee: string;
+    hiringManager: string;
+    pageTitle: string;
+  };
   profile: {
-    about: string
-    edit: string
-    add: string
-    inProgress: string
-    required: string
-    fieldsComplete: string
-    personalInformation: { title: string, linkLabel: string } 
-    employment: { title: string, linkLabel: string } 
-    referral: { title: string, linkLabel: string } 
-    qualifications: { title: string, linkLabel: string } 
-    back: string
-  },
+    about: string;
+    edit: string;
+    add: string;
+    inProgress: string;
+    required: string;
+    fieldsComplete: string;
+    personalInformation: { title: string; linkLabel: string };
+    employment: { title: string; linkLabel: string };
+    referral: { title: string; linkLabel: string };
+    qualifications: { title: string; linkLabel: string };
+    back: string;
+  };
   form: {
-    save: string
-    submit: string
-  },
+    save: string;
+    submit: string;
+  };
   personalInformation: {
-    pageTitle: string
-    preferredLanguage: string
-    workEmail: string
-    personalEmail: string
-    workPhone: string
-    personalPhone: string
-    education: string
-    additionalInformation: string
-    additionalInfoHelpMessage: string
+    pageTitle: string;
+    preferredLanguage: string;
+    workEmail: string;
+    personalEmail: string;
+    workPhone: string;
+    personalPhone: string;
+    education: string;
+    additionalInformation: string;
+    additionalInfoHelpMessage: string;
     errors: {
-      preferredLanguage_Required: string
-      personalEmail_Invalid: string
-      personalEmail_Required: string
-      workPhone_Invalid: string
-      workPhone_Required: string
-      personalPhone_Invalid: string
-      personalPhone_Required: string
-      education_Required: string
-    }
-  },
+      preferredLanguage_Required: string;
+      personalEmail_Invalid: string;
+      personalEmail_Required: string;
+      workPhone_Invalid: string;
+      workPhone_Required: string;
+      personalPhone_Invalid: string;
+      personalPhone_Required: string;
+      education_Required: string;
+    };
+  };
   employmentInformation: {
-    pageTitle: string
-    personalRecordIdentifier: string
-    substantivePositionHeading: string
-    substantivePositionGroupAndLevel: string
-    substantiveGroup: string
-    substantiveLevel: string
-    branchOrServiceCanadaRegion: string
-    directorate: string
-    provinces: string
-    city: string
-    wfaDetailsHeading: string
-    current_Wfa_Status: string
-    wfa_EffectiveDate: string
-    wfa_EndDate: string
-    hrAdvisor: string
+    pageTitle: string;
+    personalRecordIdentifier: string;
+    substantivePositionHeading: string;
+    substantivePositionGroupAndLevel: string;
+    substantiveGroup: string;
+    substantiveLevel: string;
+    branchOrServiceCanadaRegion: string;
+    directorate: string;
+    provinces: string;
+    city: string;
+    wfaDetailsHeading: string;
+    current_Wfa_Status: string;
+    wfa_EffectiveDate: string;
+    wfa_EndDate: string;
+    hrAdvisor: string;
     errors: {
-      personalRecordIdentifier_Required: string
-      personalRecordIdentifier_Invalid: string
-      substantiveGroup_Required: string
-      substantiveLevel_Required: string
-      branchOrServiceCanadaRegion_Required: string
-      directorate_Required: string
-      provinces_Required: string
-      city_Required: string
-      current_Wfa_Status_Required: string
-      hrAdvisor_Required: string
-    }
-  }
+      personalRecordIdentifier_Required: string;
+      personalRecordIdentifier_Invalid: string;
+      substantiveGroup_Required: string;
+      substantiveLevel_Required: string;
+      branchOrServiceCanadaRegion_Required: string;
+      directorate_Required: string;
+      provinces_Required: string;
+      city_Required: string;
+      current_Wfa_Status_Required: string;
+      hrAdvisor_Required: string;
+    };
+  };
 }
