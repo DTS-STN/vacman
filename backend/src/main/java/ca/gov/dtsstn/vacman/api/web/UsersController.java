d529a82295ddcbf867bef0b540fedbdeaced217c
Jordan <jordan.walcaraz@gmail.com>
2025-06-04 09:27:40 -0400

    fix(controller): import missing HttpStatus and ResponseStatusException for error handling

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/UsersController.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/UsersController.java
@@ -4,6 +4,7 @@ import org.hibernate.validator.constraints.Range;
 import org.mapstruct.factory.Mappers;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import ca.gov.dtsstn.vacman.api.config.SpringDocConfig;
 import ca.gov.dtsstn.vacman.api.service.UserService;
