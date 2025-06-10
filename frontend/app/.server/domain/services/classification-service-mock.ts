b01ca0947fa9bc2b674e876d3d9ab0f915338d56
Faiza Jahanzeb <faiza.jahanzeb@hrsdc-rhdcc.gc.ca>
2025-06-10 10:17:58 -0400

    update classification service

--- frontend/app/.server/domain/services/classification-service-mock.ts
+++ frontend/app/.server/domain/services/classification-service-mock.ts
@@ -1,3 +1,6 @@
+import type { Result, Option } from 'oxide.ts';
+import { Err, None, Ok, Some } from 'oxide.ts';
+
 import type { Classification } from '~/.server/domain/models';
 import type { ClassificationService } from '~/.server/domain/services/classification-service';
 import esdcClassificationData from '~/.server/resources/classification.json';
@@ -6,8 +9,9 @@ import { ErrorCodes } from '~/errors/error-codes';
 
 export function getMockClassificationService(): ClassificationService {
   return {
-    getClassifications: () => Promise.resolve(getClassifications()),
-    getClassificationById: (id: string) => Promise.resolve(getClassificationById(id)),
+    getAll: () => Promise.resolve(getAll()),
+    getById: (id: string) => Promise.resolve(getById(id)),
+    findById: (id: string) => Promise.resolve(findById(id)),
   };
 }
 
@@ -16,24 +20,51 @@ export function getMockClassificationService(): ClassificationService {
  *
  * @returns An array of esdc classification objects.
  */
-function getClassifications(): readonly Classification[] {
-  return esdcClassificationData.content.map((classification) => ({
+function getAll(): Result<readonly Classification[], AppError> {
+  const classifications: Classification[] = esdcClassificationData.content.map((classification) => ({
     id: classification.id.toString(),
     name: classification.name,
   }));
+
+  return Ok(classifications);
 }
 
 /**
  * Retrieves a single esdc classification group and level by its ID.
  *
  * @param id The ID of the esdc classification to retrieve.
- * @returns The esdc classification object if found.
- * @throws {AppError} If the esdc classification is not found.
+ * @returns The esdc classification object if found or {AppError} If the esdc classification is not found.
  */
-function getClassificationById(id: string): Classification {
-  const classification = getClassifications().find((cg) => cg.id === id);
-  if (!classification) {
-    throw new AppError(`Classification with ID '${id}' not found.`, ErrorCodes.NO_CLASSIFICATION_FOUND);
+function getById(id: string): Result<Classification, AppError> {
+  const result = getAll();
+
+  if (result.isErr()) {
+    return result;
   }
-  return classification;
+
+  const classifications = result.unwrap();
+  const classification = classifications.find((p) => p.id === id);
+
+  return classification
+    ? Ok(classification)
+    : Err(new AppError(`Classification with ID '${id}' not found.`, ErrorCodes.NO_CLASSIFICATION_FOUND));
+}
+
+/**
+ * Retrieves a single esdc classification group and level by its ID.
+ *
+ * @param id The ID of the esdc classification to retrieve.
+ * @returns The esdc classification object if found or undefined if not found.
+ */
+function findById(id: string): Option<Classification> {
+  const result = getAll();
+
+  if (result.isErr()) {
+    return None;
+  }
+
+  const classifications = result.unwrap();
+  const classification = classifications.find((p) => p.id === id);
+
+  return classification ? Some(classification) : None;
 }
