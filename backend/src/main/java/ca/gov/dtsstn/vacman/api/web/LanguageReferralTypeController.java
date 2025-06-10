f38223dc4992c48e829cdb3a36b56d338cb4e8f0
alex-solo <alex.soloviev@gmail.com>
2025-06-10 15:10:44 -0700

    add language referral types endpoint

---
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/LanguageReferralTypeController.java
@@ -0,0 +1,54 @@
+package ca.gov.dtsstn.vacman.api.web;
+
+import java.util.List;
+
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ca.gov.dtsstn.vacman.api.config.SpringDocConfig;
+import ca.gov.dtsstn.vacman.api.service.LanguageReferralTypeService;
+import ca.gov.dtsstn.vacman.api.web.model.LanguageReferralTypeReadModel;
+import ca.gov.dtsstn.vacman.api.web.model.mapper.LanguageReferralTypeModelMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "Language Referral Types")
+@RequestMapping({ "/api/v1/language-referral-types" })
+public class LanguageReferralTypeController {
+
+    private final LanguageReferralTypeModelMapper languageReferralTypeModelMapper = Mappers.getMapper(LanguageReferralTypeModelMapper.class);
+
+    private final LanguageReferralTypeService languageReferralTypeService;
+
+    public LanguageReferralTypeController(LanguageReferralTypeService languageReferralTypeService) {
+        this.languageReferralTypeService = languageReferralTypeService;
+    }
+
+    @GetMapping
+    @SecurityRequirement(name = SpringDocConfig.AZURE_AD)
+    @Operation(summary = "Get all language referral types or filter by code.", description = "Returns a list of all language referral types or a specific language referral type if code is provided.")
+    public List<LanguageReferralTypeReadModel> getLanguageReferralTypes(
+            @RequestParam(required = false)
+            @Parameter(description = "Language referral type code to filter by")
+            String code) {
+        
+        if (code != null && !code.isEmpty()) {
+            return languageReferralTypeService.getLanguageReferralTypeByCode(code)
+                .map(languageReferralTypeModelMapper::toModel)
+                .map(List::of)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Language referral type with code '" + code + "' not found"));
+        }
+        
+        return languageReferralTypeService.getAllLanguageReferralTypes().stream()
+            .map(languageReferralTypeModelMapper::toModel)
+            .toList();
+    }
+}
