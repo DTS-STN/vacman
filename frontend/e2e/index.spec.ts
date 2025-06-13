1801bf5acfed10c404b74e5f1c57e1b3af2f257d
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-06-13 06:27:20 -0230

    fix(frontend): remove broken e2e test

--- frontend/e2e/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('Navigating to / redirects to the default language page', async ({ page }) => {
-  await page.goto('/');
-
-  expect(await page.locator('main').innerHTML()).toMatchSnapshot();
-});
