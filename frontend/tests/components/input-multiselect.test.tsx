df8d84a3bdefd14369d8c48fdcd89e3880cda83a
Faiza Jahanzeb <faiza.jahanzeb@hrsdc-rhdcc.gc.ca>
2025-06-25 15:24:08 -0400

    add test library

--- frontend/tests/components/input-multiselect.test.tsx
+++ frontend/tests/components/input-multiselect.test.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps } from 'react';
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { vi, describe, it, expect, beforeEach } from 'vitest';
 
 import { InputMultiSelect } from '~/components/input-multiselect';
