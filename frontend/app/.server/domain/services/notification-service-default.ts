ee89a1f26b07efd69bc653c513cf79c0bd7c641d
Marco <marcogoc951@gmail.com>
2025-07-10 20:33:13 -0400

    gc-notify v2 after format write

--- frontend/app/.server/domain/services/notification-service-default.ts
+++ frontend/app/.server/domain/services/notification-service-default.ts
@@ -28,8 +28,6 @@ export function getDefaultNotificationService(): NotificationService {
           headers: {
             'Authorization': `ApiKey-v1 ${serverEnvironment.GC_NOTIFY_API_KEY}`,
             'Content-Type': 'application/json',
-            // uncommenting next line creates an error 
-            //'Ocp-Apim-Subscription-Key': serverEnvironment.INTEROP_API_SUBSCRIPTION_KEY,
           },
           body: JSON.stringify({ emailData }),
         });
