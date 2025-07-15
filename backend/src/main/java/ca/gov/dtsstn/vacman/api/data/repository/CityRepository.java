e45ff4cd968733116ee87bbcbe1521f0a2e6c6ac
Jordan <jordan.walcaraz@gmail.com>
2025-07-15 14:51:55 -0400

    refactor: update CityRepository and CityReadModel for consistency in province/territory naming

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/data/repository/CityRepository.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/data/repository/CityRepository.java
@@ -1,19 +1,20 @@
 package ca.gov.dtsstn.vacman.api.data.repository;
 
-import ca.gov.dtsstn.vacman.api.data.entity.CityEntity;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import ca.gov.dtsstn.vacman.api.data.entity.CityEntity;
 
 @Repository
 public interface CityRepository extends ListCrudRepository<CityEntity, Long> {
     Optional<CityEntity> findByCode(String code);
 
-    List<CityEntity> findByProvinceCode(String provinceCode);
+    List<CityEntity> findByProvinceTerritoryCode(String provinceCode);
 
-    List<CityEntity> findByProvince_Id(Long provinceId);
+    List<CityEntity> findByCodeAndProvinceTerritoryCode(String code, String provinceCode);
 
-    List<CityEntity> findByCodeAndProvinceCode(String code, String provinceCode);
+    List<CityEntity> findByProvinceTerritory_Id(Long provinceId);
 }
