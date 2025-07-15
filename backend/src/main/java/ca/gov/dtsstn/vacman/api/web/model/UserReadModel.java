19705483ace8452e81694eb74bb065367898a005
Jordan <jordan.walcaraz@gmail.com>
2025-07-15 13:04:37 -0400

    feat: update UserCreateModel and UserReadModel to enhance user data representation

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/UserReadModel.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/UserReadModel.java
@@ -1,7 +1,5 @@
 package ca.gov.dtsstn.vacman.api.web.model;
 
-import java.time.Instant;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 
@@ -10,8 +8,8 @@ public record UserReadModel(
 	@Schema(accessMode = AccessMode.READ_ONLY, description = "The unique identifier for this user.", example = "1")
 	Long id,
 
-	@Schema(description = "The role of this user.", example = "employee")
-	String role,
+	@Schema(description = "The type of this user.")
+	UserTypeReadModel userType,
 
 	@Schema(description = "The network name of this user.", example = "user@example.com")
 	String networkName,
@@ -40,15 +38,18 @@ public record UserReadModel(
 	@Schema(description = "The business email of this user.", example = "john.doe@example.com")
 	String businessEmail,
 
+	@Schema(description = "The language associated with this user.")
+	LanguageReadModel language,
+
 	@Schema(accessMode = AccessMode.READ_ONLY, description = "The user or service that created this user.", example = "vacman-api")
-	String userCreated,
+	String createdBy,
 
 	@Schema(accessMode = AccessMode.READ_ONLY, description = "The time this user was created.", example = "2000-01-01T00:00:00Z")
-	String dateCreated,
+	String createdDate,
 
 	@Schema(accessMode = AccessMode.READ_ONLY, description = "The user or service that last modified this user.", example = "vacman-api")
-	String userUpdated,
+	String lastModifiedBy,
 
 	@Schema(accessMode = AccessMode.READ_ONLY, description = "The time this user was last modified.", example = "2000-01-01T00:00:00Z")
-	String dateUpdated
+	String lastModifiedDate
 ) {}
