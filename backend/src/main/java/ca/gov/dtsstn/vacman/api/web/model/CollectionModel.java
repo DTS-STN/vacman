ca74e3e1a4b3be4a8decc8e851fde6923e105396
Jordan <jordan.walcaraz@gmail.com>
2025-07-15 08:46:44 -0400

    refactor: Rename field 'data' to 'content' in CollectionModel for consistency

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/CollectionModel.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/CollectionModel.java
@@ -5,5 +5,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "A collection of items.")
 public record CollectionModel<T>(
 	@Schema(description = "The data contained in this collection.")
-	Iterable<T> data
+	Iterable<T> content
 ) {}
