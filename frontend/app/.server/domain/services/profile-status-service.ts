5c976a36092a678967ccac408fec680f81979d70
Shawn Zhang <xuan.zhang@hrsdc-rhdcc.gc.ca>
2025-07-29 12:40:38 -0400

    AB#6436 Add profile status mock (#273)
    * add profile status mock
    
    * update profile status service
    
    * update sql
    
    * update profile state in data.sql
    
    * update
    
    * update json

---
+++ frontend/app/.server/domain/services/profile-status-service.ts
@@ -0,0 +1,20 @@
+import type { Result, Option } from 'oxide.ts';
+
+import { getDefaultProfileStatusService } from './profile-status-service-default';
+import { getMockProfileStatusService } from './profile-status-service-mock';
+
+import type { LocalizedProfileStatus, ProfileStatus } from '~/.server/domain/models';
+import { serverEnvironment } from '~/.server/environment';
+import type { AppError } from '~/errors/app-error';
+
+export type ProfileStatusService = {
+  listAll(): Promise<readonly ProfileStatus[]>;
+  listAllLocalized(language: Language): Promise<readonly LocalizedProfileStatus[]>;
+  getById(id: string): Promise<Result<ProfileStatus, AppError>>;
+  getLocalizedById(id: string, language: Language): Promise<Result<LocalizedProfileStatus, AppError>>;
+  findLocalizedById(id: string, language: Language): Promise<Option<LocalizedProfileStatus>>;
+};
+
+export function getProfileStatusService(): ProfileStatusService {
+  return serverEnvironment.ENABLE_LOOKUP_FIELD_SERVICES_MOCK ? getMockProfileStatusService() : getDefaultProfileStatusService();
+}
