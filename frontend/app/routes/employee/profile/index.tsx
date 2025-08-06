a37c2cd4b82f28b0fd7fe42cf776a79afe27489c
Shawn Zhang <xuan.zhang@hrsdc-rhdcc.gc.ca>
2025-08-06 15:08:21 -0400

    refactor(frontend) check privacy consent on profile page (#338)
    * add privacy check on profile page
    
    * check privacy consent on profile page
    
    * update route
    
    * update
    
    * use requirePrivacConsentForOwnProfile
    
    * remove checkEmployeeRoutePrivacyConsent

--- frontend/app/routes/employee/profile/index.tsx
+++ frontend/app/routes/employee/profile/index.tsx
@@ -20,6 +20,7 @@ import { getProfileStatusService } from '~/.server/domain/services/profile-statu
 import { getUserService } from '~/.server/domain/services/user-service';
 import { getWFAStatuses } from '~/.server/domain/services/wfa-status-service';
 import type { AuthenticatedSession } from '~/.server/utils/auth-utils';
+import { requirePrivacyConsentForOwnProfile } from '~/.server/utils/privacy-consent-utils';
 import { countCompletedItems, omitObjectProperties } from '~/.server/utils/profile-utils';
 import { AlertMessage } from '~/components/alert-message';
 import { Button } from '~/components/button';
@@ -106,6 +107,9 @@ export async function action({ context, request }: Route.ActionArgs) {
 export async function loader({ context, request, params }: Route.LoaderArgs) {
   // Use the id parameter from the URL to fetch the profile
   const profileUserId = params.id;
+
+  const currentUrl = new URL(request.url);
+  await requirePrivacyConsentForOwnProfile(context.session as AuthenticatedSession, profileUserId, currentUrl);
 
   const { lang, t } = await getTranslation(request, handle.i18nNamespace);
 
