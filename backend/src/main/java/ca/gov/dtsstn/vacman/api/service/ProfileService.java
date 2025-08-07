dd606fe1c80e29b0858667e80ed10794b6f86b6b
Maxwell Haley <maxwell.haley@hrsdc-rhdcc.gc.ca>
2025-08-07 10:25:48 -0400

    refactor(backend): Adjust to new query params

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/service/ProfileService.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/service/ProfileService.java
@@ -2,11 +2,11 @@ package ca.gov.dtsstn.vacman.api.service;
 
 import ca.gov.dtsstn.vacman.api.data.entity.ProfileEntity;
 import ca.gov.dtsstn.vacman.api.data.repository.ProfileRepository;
-import ca.gov.dtsstn.vacman.api.web.model.ProfilesStatusParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -14,10 +14,11 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
 
-    // Hardcoded profile code-codes that get reference based on the status code used for filtering in the repo codegen.
-    // That's 5 degrees of coding.
-    private final Set<String> activeProfileCodes = Set.of("APPROVED", "PENDING", "INCOMPLETE");
-    private final Set<String> inactiveProfileCodes = Set.of("ARCHIVED");
+    // Keys are tied to the potential values of getProfilesByStatusAndHrId parameter isActive.
+    private final Map<Boolean, Set<String>> profileStatusSets = Map.of(
+            Boolean.TRUE, Set.of("APPROVED", "PENDING", "INCOMPLETE"),
+            Boolean.FALSE, Set.of("ARCHIVED")
+    );
 
     public ProfileService(ProfileRepository profileRepository) {
         this.profileRepository = profileRepository;
@@ -28,18 +29,22 @@ public class ProfileService {
      * allows for the HR Advisor's ID to be used as an additional filter.
      *
      * @param pageRequest Pagination information.
-     * @param status The target status for filtering.
+     * @param isActive Filter return based on active status.
      * @param hrAdvisorId Optional ID for additional filtering. Can be set to {@code null}.
      * @return A paginated collection of profile entities.
      */
-    public Page<ProfileEntity> getProfilesByStatusAndHrId(PageRequest pageRequest, ProfilesStatusParam status, Long hrAdvisorId) {
-        final var statusCodes = switch (status) {
-            case ACTIVE -> activeProfileCodes;
-            case INACTIVE -> inactiveProfileCodes;
-        };
+    public Page<ProfileEntity> getProfilesByStatusAndHrId(PageRequest pageRequest, Boolean isActive, Long hrAdvisorId) {
+        // Dispatch DB call based on presence of isActive & advisor ID
+        if (isActive != null) {
+            final var statusCodes = profileStatusSets.get(isActive);
 
-        return (hrAdvisorId == null)
-                ? profileRepository.findByProfileStatusCodeIn(statusCodes, pageRequest)
-                : profileRepository.findByProfileStatusCodeInAndHrAdvisorIdIs(statusCodes, hrAdvisorId, pageRequest);
+            return (hrAdvisorId == null)
+                    ? profileRepository.findByProfileStatusCodeIn(statusCodes, pageRequest)
+                    : profileRepository.findByProfileStatusCodeInAndHrAdvisorIdIs(statusCodes, hrAdvisorId, pageRequest);
+        } else {
+            return (hrAdvisorId == null)
+                    ? profileRepository.findAll(pageRequest)
+                    : profileRepository.findAllByHrAdvisorId(hrAdvisorId, pageRequest);
+        }
     }
 }
