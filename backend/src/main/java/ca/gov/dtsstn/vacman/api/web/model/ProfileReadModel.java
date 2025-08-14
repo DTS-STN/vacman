51cde1d22eda968aa16f08b4859b9de340c063dc
Maxwell Haley <maxwell.haley@hrsdc-rhdcc.gc.ca>
2025-08-14 11:28:14 -0400

    refactor(backend): Update ProfileReadModel

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/ProfileReadModel.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/ProfileReadModel.java
@@ -1,6 +1,7 @@
 package ca.gov.dtsstn.vacman.api.web.model;
 
 import java.time.Instant;
+import java.util.Set;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -8,35 +9,29 @@ public record ProfileReadModel(
 		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The unique identifier for this profile.", example = "1")
 		Long id,
 
-		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The unique identifier for the user associated with this profile.", example = "2")
-		Long userId,
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The user associated with this profile")
+		UserReadModel profileUser,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The unique identifier for the HR advisor associated with this profile.", example = "2")
 		Long hrAdvisorId,
 
-		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The Microsoft Entra ID of the user associated with this profile.", example = "00000000-0000-0000-0000-000000000000")
-		String microsoftEntraId,
-
-		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The first name of the user associated with this profile..", example = "John")
-		String firstName,
-
-		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The last name of the user associated with this profile.", example = "Doe")
-		String lastName,
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The personal phone number of this profile.", example = "5550-000-1234")
+		String personalPhoneNumber,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The personal email of this profile.", example = "john.doe@example.com")
 		String personalEmailAddress,
 
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The city associated with the user associated with this profile.")
+		CityReadModel substantiveCity,
+
 		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The language associated with the user associated with this profile.")
-		LanguageReadModel language,
+		LanguageReadModel languageOfCorrespondence,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The classification of this profile.")
-		ClassificationReadModel classification,
-
-		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The priority of this profile.")
-		PriorityLevelReadModel priorityLevel,
+		ClassificationReadModel substantiveClassification,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The work unit of this profile.")
-		WorkUnitReadModel workUnit,
+		WorkUnitReadModel substantiveWorkUnit,
 
 		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The status of this profile.")
 		ProfileStatusReadModel profileStatus,
@@ -44,11 +39,32 @@ public record ProfileReadModel(
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "The WFA status of this profile.")
 		WfaStatusReadModel wfaStatus,
 
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The start date of the WFA.")
+		Instant wfaStartDate,
+
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The end date of the WFA.")
+		Instant wfaEndDate,
+
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "Is this profile available for referral?")
 		Boolean isAvailableForReferral,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "Is this profile available for alternation?", example = "true")
 		Boolean isInterestedInAlternation,
+
+		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "Has the user of this profile consented to the privacy policy?", example = "true")
+		Boolean hasConsentedToPrivacyTerms,
+
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The profiles preferred cities for employment.")
+		Set<CityReadModel> preferredCities,
+
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The profiles preferred classifications for employment.")
+		Set<ClassificationReadModel> preferredClassifications,
+
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The profiles preferred opportunities for employment.")
+		Set<EmploymentOpportunityReadModel> preferredEmploymentOpportunities,
+
+		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The profiles preferred languages for employment.")
+		Set<LanguageReferralTypeReadModel> preferredLanguages,
 
 		@Schema(accessMode = Schema.AccessMode.READ_WRITE, description = "Additional comments on this profile.", example = "One cool individual.")
 		String additionalComment,
@@ -64,4 +80,12 @@ public record ProfileReadModel(
 
 		@Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "The time this user was last modified.", example = "2000-01-01T00:00:00Z")
 		Instant lastModifiedDate
-		) {}
+		) {
+
+	public record UserModel(
+			Long id,
+			String firstName,
+			String lastName,
+			String emailAddress
+	) {}
+}
--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/mapper/ProfileModelMapper.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/mapper/ProfileModelMapper.java
@@ -1,23 +1,38 @@
 package ca.gov.dtsstn.vacman.api.web.model.mapper;
 
+import ca.gov.dtsstn.vacman.api.data.entity.*;
+import ca.gov.dtsstn.vacman.api.web.model.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import ca.gov.dtsstn.vacman.api.data.entity.ProfileEntity;
-import ca.gov.dtsstn.vacman.api.web.model.ProfileReadModel;
 
 @Mapper(uses = { CodeModelMapper.class })
 public interface ProfileModelMapper {
 
-	@Mapping(source = "user.id", target = "userId")
-	@Mapping(source = "user.microsoftEntraId", target = "microsoftEntraId")
-	@Mapping(source = "user.firstName", target = "firstName")
-	@Mapping(source = "user.lastName", target = "lastName")
+	@Mapping(source = "user", target = "profileUser")
 	@Mapping(source = "hrAdvisor.id", target = "hrAdvisorId")
+	@Mapping(source = "city", target = "substantiveCity")
+	@Mapping(source = "language", target = "languageOfCorrespondence")
+	@Mapping(source = "classification", target = "substantiveClassification")
+	@Mapping(source = "workUnit", target = "substantiveWorkUnit")
+	@Mapping(source = "profileCities", target = "preferredCities")
+	@Mapping(source = "classificationProfiles", target = "preferredClassifications")
+	@Mapping(source = "employmentOpportunities", target = "preferredEmploymentOpportunities")
+	@Mapping(source = "languageReferralTypes", target = "preferredLanguages")
 	ProfileReadModel toModel(ProfileEntity entity);
 
-	@Mapping(source = "user.id", target = "userId")
+	@Mapping(source = "user.id", target = "profileUser.id")
 	@Mapping(source = "hrAdvisor.id", target = "hrAdvisorId")
 	ProfileReadModel toModelNoUserData(ProfileEntity entity);
 
+	@Mapping(source = "city", target = ".")
+	CityReadModel toCityReadModel(ProfileCityEntity entity);
+
+	@Mapping(source = "classification", target = ".")
+	ClassificationReadModel toClassificationReadModel(ClassificationProfileEntity entity);
+
+	@Mapping(source = "employmentOpportunity", target = ".")
+	EmploymentOpportunityReadModel toEmploymentOpportunityReadModel(ProfileEmploymentOpportunityEntity entity);
+
+	@Mapping(source = "languageReferralType", target = ".")
+	LanguageReferralTypeReadModel toLanguageReferralTypeReadModel(ProfileLanguageReferralTypeEntity entity);
 }
