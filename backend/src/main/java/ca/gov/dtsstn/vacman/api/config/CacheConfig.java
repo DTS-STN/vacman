a47b18beb84fbbf3a196920b31004d06c7d15b28
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-08-14 10:04:00 -0230

    fix(backend): remove public modifiers from cache config

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/config/CacheConfig.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/config/CacheConfig.java
@@ -13,139 +13,121 @@ import ca.gov.dtsstn.vacman.api.config.cache.CaffeineCacheFactory;
 @EnableCaching
 @Configuration
 @ConditionalOnProperty(name = { "application.caching.enabled" })
-public class CacheConfig {
+class CacheConfig {
 
 	private static final Logger log = LoggerFactory.getLogger(CacheConfig.class);
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.cities")
-	public CaffeineCacheFactory cityCache() {
+	@Bean CaffeineCacheFactory cityCache() {
 		log.info("Creating 'cityCache' bean");
 		return new CaffeineCacheFactory("cities");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.classifications")
-	public CaffeineCacheFactory classificationCache() {
+	@Bean CaffeineCacheFactory classificationCache() {
 		log.info("Creating 'classificationCache' bean");
 		return new CaffeineCacheFactory("classifications");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.employment-equities")
-	public CaffeineCacheFactory employmentEquityCache() {
+	@Bean CaffeineCacheFactory employmentEquityCache() {
 		log.info("Creating 'employmentEquityCache' bean");
 		return new CaffeineCacheFactory("employment-equities");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.employment-opportunities")
-	public CaffeineCacheFactory employmentOpportunityCache() {
+	@Bean CaffeineCacheFactory employmentOpportunityCache() {
 		log.info("Creating 'employmentOpportunityCache' bean");
 		return new CaffeineCacheFactory("employment-opportunities");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.employment-tenures")
-	public CaffeineCacheFactory employmentTenureCache() {
+	@Bean CaffeineCacheFactory employmentTenureCache() {
 		log.info("Creating 'employmentTenureCache' bean");
 		return new CaffeineCacheFactory("employment-tenures");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.languages")
-	public CaffeineCacheFactory languageCache() {
+	@Bean CaffeineCacheFactory languageCache() {
 		log.info("Creating 'languageCache' bean");
 		return new CaffeineCacheFactory("languages");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.language-referral-types")
-	public CaffeineCacheFactory languageReferralTypeCache() {
+	@Bean CaffeineCacheFactory languageReferralTypeCache() {
 		log.info("Creating 'languageReferralTypeCache' bean");
 		return new CaffeineCacheFactory("language-referral-types");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.language-requirements")
-	public CaffeineCacheFactory languageRequirementCache() {
+	@Bean CaffeineCacheFactory languageRequirementCache() {
 		log.info("Creating 'languageRequirementCache' bean");
 		return new CaffeineCacheFactory("language-requirements");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.non-advertised-appointments")
-	public CaffeineCacheFactory nonAdvertisedAppointmentCache() {
+	@Bean CaffeineCacheFactory nonAdvertisedAppointmentCache() {
 		log.info("Creating 'nonAdvertisedAppointmentCache' bean");
 		return new CaffeineCacheFactory("non-advertised-appointments");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.priority-levels")
-	public CaffeineCacheFactory priorityLevelCache() {
+	@Bean CaffeineCacheFactory priorityLevelCache() {
 		log.info("Creating 'priorityLevelCache' bean");
 		return new CaffeineCacheFactory("priority-levels");
 	}
 
 	@Bean
 	@ConfigurationProperties("application.caching.caches.profile-statuses")
-	public CaffeineCacheFactory profileStatusCache() {
+	CaffeineCacheFactory profileStatusCache() {
 		log.info("Creating 'profileStatusCache' bean");
 		return new CaffeineCacheFactory("profile-statuses");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.provinces")
-	public CaffeineCacheFactory provinceCache() {
+	@Bean CaffeineCacheFactory provinceCache() {
 		log.info("Creating 'provinceCache' bean");
 		return new CaffeineCacheFactory("provinces");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.request-statuses")
-	public CaffeineCacheFactory requestStatusCache() {
+	@Bean CaffeineCacheFactory requestStatusCache() {
 		log.info("Creating 'requestStatusCache' bean");
 		return new CaffeineCacheFactory("request-statuses");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.security-clearances")
-	public CaffeineCacheFactory securityClearanceCache() {
+	@Bean CaffeineCacheFactory securityClearanceCache() {
 		log.info("Creating 'securityClearanceCache' bean");
 		return new CaffeineCacheFactory("security-clearances");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.selection-process-types")
-	public CaffeineCacheFactory selectionProcessTypeCache() {
+	@Bean CaffeineCacheFactory selectionProcessTypeCache() {
 		log.info("Creating 'selectionProcessTypeCache' bean");
 		return new CaffeineCacheFactory("selection-process-types");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.user-types")
-	public CaffeineCacheFactory userTypeCache() {
+	@Bean CaffeineCacheFactory userTypeCache() {
 		log.info("Creating 'userTypeCache' bean");
 		return new CaffeineCacheFactory("user-types");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.wfa-statuses")
-	public CaffeineCacheFactory wfaStatusCache() {
+	@Bean CaffeineCacheFactory wfaStatusCache() {
 		log.info("Creating 'wfaStatusCache' bean");
 		return new CaffeineCacheFactory("wfa-statuses");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.work-schedules")
-	public CaffeineCacheFactory workScheduleCache() {
+	@Bean CaffeineCacheFactory workScheduleCache() {
 		log.info("Creating 'workScheduleCache' bean");
 		return new CaffeineCacheFactory("work-schedules");
 	}
 
-	@Bean
 	@ConfigurationProperties("application.caching.caches.work-units")
-	public CaffeineCacheFactory workUnitCache() {
+	@Bean CaffeineCacheFactory workUnitCache() {
 		log.info("Creating 'workUnitCache' bean");
 		return new CaffeineCacheFactory("work-units");
 	}
