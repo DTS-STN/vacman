74514d7e8e37a3c86e053c295c3565f762b99110
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2025-08-19 16:20:42 -0400

    Fixed wfaStatus error not appearing

--- frontend/app/routes/page-components/employees/employment-information/form.tsx
+++ frontend/app/routes/page-components/employees/employment-information/form.tsx
@@ -215,7 +215,7 @@ export function EmploymentInformationForm({
               <InputRadios
                 id="wfaStatus"
                 name="wfaStatus"
-                errorMessage={t(extractValidationKey(formErrors?.wfaStatus))}
+                errorMessage={t(extractValidationKey(formErrors?.wfaStatusId))}
                 required
                 options={wfaStatusOptions}
                 legend={t('employment-information.wfa-status')}
