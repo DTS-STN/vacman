b403bf6baaa87c137a01a3d4dc2a3c5dd0d0cfca
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-09-05 11:03:43 -0230

    fix(frontend): disable automatic .env parsing in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   build: {
     target: 'es2022',
   },
+  envDir: false,
   optimizeDeps: {
     entries: ['./app/entry.client.tsx', './app/root.tsx', './app/routes/**/*.tsx'],
   },
