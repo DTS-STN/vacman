3e8726987db92814eafb7250612f4e0d6fdb4fd6
Faiza Jahanzeb <faiza.jahanzeb@hrsdc-rhdcc.gc.ca>
2025-09-23 12:45:23 -0400

    removed

--- frontend/app/components/status-tag.tsx
+++ frontend/app/components/status-tag.tsx
@@ -3,7 +3,7 @@ import type { JSX } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import type { RequestStatus } from '~/.server/domain/models';
-import { PROFILE_STATUS, REQUEST_STATUS_CODE } from '~/domain/constants';
+import { PROFILE_STATUS } from '~/domain/constants';
 import { cn } from '~/utils/tailwind-utils';
 
 interface StatusTagProps {
@@ -20,11 +20,8 @@ function getStatusStyle(code: string): string {
     case PROFILE_STATUS.ARCHIVED.code:
       return 'border-orange-400 bg-orange-100 text-orange-800';
     case PROFILE_STATUS.INCOMPLETE.code:
-    case REQUEST_STATUS_CODE.SUBMIT:
       return 'border-blue-400 bg-blue-100 text-blue-800';
     case PROFILE_STATUS.PENDING.code:
-    case REQUEST_STATUS_CODE.FDBK_PEND_APPR:
-    case REQUEST_STATUS_CODE.DRAFT:
       return 'border-yellow-400 bg-yellow-100 text-yellow-800';
     default:
       return '';
