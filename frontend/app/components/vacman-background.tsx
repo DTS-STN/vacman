2eaf379a51c9393e6697879a0648c0cf95c08a2e
Dario Au <dario.au@hrsdc-rhdcc.gc.ca>
2025-09-24 11:37:52 -0600

    refactor(frontend): remove manual heights from vacman-background (#889)
    refactor(frontend): refactor vacman-background component to not require manual heights

--- frontend/app/components/vacman-background.tsx
+++ frontend/app/components/vacman-background.tsx
@@ -3,19 +3,8 @@ import type { ComponentProps } from 'react';
 import { cn } from '~/utils/tailwind-utils';
 
 export interface VacmanBackgroundProps extends ComponentProps<'div'> {
-  height?: 'h-30' | 'h-40' | 'h-50' | 'h-60' | 'h-70' | 'h-80' | 'h-90';
   variant?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
 }
-
-const smallViewportHeights = {
-  ['h-30']: 'h-40',
-  ['h-40']: 'h-50',
-  ['h-50']: 'h-60',
-  ['h-60']: 'h-70',
-  ['h-70']: 'h-80',
-  ['h-80']: 'h-90',
-  ['h-90']: 'h-100',
-};
 
 const variants = {
   ['top-left']: 'scale-x-[1] scale-y-[-1]',
@@ -24,19 +13,25 @@ const variants = {
   ['bottom-right']: 'scale-x-[-1] scale-y-[1]',
 };
 
-export function VacmanBackground({ className, children, height = 'h-60', variant = 'top-left' }: VacmanBackgroundProps) {
+export function VacmanBackground({ className, children, variant = 'top-left' }: VacmanBackgroundProps) {
   return (
-    <div className={cn('flex text-white', className, `sm:${height}`, smallViewportHeights[height])}>
-      <div>{children}</div>
+    <div className={cn('flex text-white', className)}>
+      <div className="my-8">{children}</div>
       <div
         role="presentation"
         className={cn(
           "absolute left-0 -z-10 w-full bg-[rgba(9,28,45,1)] bg-[url('/VacMan-design-element-06.svg')] bg-size-[450px] bg-left-bottom bg-no-repeat",
-          `sm:${height}`,
-          smallViewportHeights[height],
           variants[variant],
         )}
-      />
+      >
+        <div
+          className="pointer-events-none invisible container touch-none py-8 select-none focus:outline-none"
+          tabIndex={-1}
+          aria-hidden
+        >
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
