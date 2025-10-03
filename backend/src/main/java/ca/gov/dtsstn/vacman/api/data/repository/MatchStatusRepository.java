073f406f518b34f04bb8fb95cf65727b23ccf751
alex-solo <alex.soloviev@gmail.com>
2025-10-03 11:28:56 -0700

    add match related reepositories

---
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/data/repository/MatchStatusRepository.java
@@ -0,0 +1,8 @@
+package ca.gov.dtsstn.vacman.api.data.repository;
+
+import ca.gov.dtsstn.vacman.api.data.entity.MatchStatusEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchStatusRepository extends AbstractCodeRepository<MatchStatusEntity> {
+}
