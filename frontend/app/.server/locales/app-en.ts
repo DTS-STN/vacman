6336820eb489b377bec9797b4fac5300c3a3a5d1
Marco <52539578+MarcoGoC@users.noreply.github.com>
2025-10-08 08:06:44 -0400

    7182 (frontend) position length set to 8  (#980)
    * set size to 8 characters
    
    * set size to 8 characters
    
    * set size to 8 characters

--- frontend/app/.server/locales/app-en.ts
+++ frontend/app/.server/locales/app-en.ts
@@ -443,7 +443,7 @@ export default {
     'security-requirement': 'Security requirement',
     'errors': {
       'position-number-required': 'Position number is required.',
-      'position-number-max-length': 'Each position number must be 6 characters.',
+      'position-number-max-length': 'Each position number must be 8 characters.',
       'group-and-level-required': 'Group and level is required.',
       'title-en-required': 'Title in English is required.',
       'title-fr-required': 'Title in French is required.',
