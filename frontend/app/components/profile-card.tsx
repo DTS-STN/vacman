845608fa7437a76b337871fe8b2241fae7f1bc35
Dario Au <dario.au@hrsdc-rhdcc.gc.ca>
2025-10-09 06:46:29 -0600

    refactor(frontend): separated profile card to separate components (#996)

--- frontend/app/components/profile-card.tsx
+++ frontend/app/components/profile-card.tsx
@@ -1,4 +1,5 @@
-import type { JSX, ReactNode, Ref } from 'react';
+import type { JSX, ReactElement, ReactNode, Ref } from 'react';
+import React from 'react';
 
 import type { Params } from 'react-router';
 
@@ -11,101 +12,135 @@ import { InlineLink } from '~/components/links';
 import type { I18nRouteFile } from '~/i18n-routes';
 import { cn } from '~/utils/tailwind-utils';
 
-interface ProfileCardProps {
-  title: string;
-  linkLabel: string;
-  file: I18nRouteFile;
-  isComplete?: boolean;
-  isNew?: boolean;
-  required?: boolean;
-  children: ReactNode;
-  params?: Params;
+interface ProfileBaseProps {
   errorState?: boolean;
-  ref?: Ref<HTMLDivElement>;
-  showStatus?: boolean;
-  updated?: boolean;
-  linkType?: 'edit' | 'view';
+  children: ReactNode;
 }
 
-export function ProfileCard({
-  title,
-  linkLabel,
-  file,
-  isComplete,
-  isNew,
-  required,
-  errorState,
-  children,
-  params,
-  ref,
-  showStatus,
-  updated,
-  linkType,
-}: ProfileCardProps): JSX.Element {
-  const { t } = useTranslation('app');
+interface ProfileExtendedProps extends ProfileBaseProps {
+  children: (ReactElement<ProfileBaseProps> | false) | (ReactElement<ProfileBaseProps> | false)[];
+}
 
-  // Excluding file or linkLabel will omit the edit link footer from the card (ex. for hr-advisor requests)
+function ChildrenWithProps({ errorState, children }: ProfileExtendedProps): ReactNode {
+  if (errorState) {
+    return React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child, { ...child, errorState });
+      }
+      return child;
+    });
+  }
+  return children;
+}
 
+interface ProfileCardProps extends ProfileExtendedProps {
+  ref?: Ref<HTMLDivElement>;
+}
+
+export function ProfileCard(props: ProfileCardProps): JSX.Element {
+  const { ref, errorState } = props;
   return (
     <Card ref={ref} className={cn(errorState && 'border-b-6 border-[#C90101]', 'rounded-md p-4 sm:p-6')}>
-      <CardHeader className="p-0">
-        <div className="mb-6 grid justify-between gap-2 select-none sm:grid-cols-2">
-          <div>
-            <CardTitle className="text-2xl">{title}</CardTitle>
-          </div>
-          <div className="space-x-2 sm:ml-auto">
-            {showStatus &&
-              (isComplete ? (
-                <CompleteTag />
-              ) : (
-                <>
-                  <InProgressTag />
-                  {required && <RequiredTag />}
-                </>
-              ))}
-            {updated && <UpdatedTag />}
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent className="my-3 space-y-3 p-0">{children}</CardContent>
-
-      {linkType && (
-        <CardFooter
-          className={cn(
-            'mt-3',
-            errorState ? 'bg-red-100' : 'bg-gray-100', // Add background
-            '-mx-4 sm:-mx-6', // Pull horizontally to cancel parent padding
-            '-mb-4 sm:-mb-6', // Pull down to cancel parent bottom padding
-            'px-4 sm:px-6', // Add horizontal padding back for the content
-            'py-4', // Add vertical padding for the contents
-            'rounded-b-xs', // Re-apply bottom roundings
-          )}
-        >
-          {linkType === 'edit' && (
-            <>
-              {errorState && <h3 className="pb-4 text-lg font-bold text-[#333333]">{t('profile.field-incomplete')}</h3>}
-              <span className="flex items-center gap-x-2">
-                {errorState && <FontAwesomeIcon icon={faTriangleExclamation} className="text-red-800" />}
-                {!errorState && (isNew ? <FontAwesomeIcon icon={faPlus} /> : <FontAwesomeIcon icon={faPenToSquare} />)}
-                <InlineLink className={`${errorState && 'text-red-800'} font-semibold`} file={file} params={params}>
-                  {`${isNew ? t('profile.add') : t('profile.edit')}\u0020`}
-                  {linkLabel}
-                </InlineLink>
-              </span>
-            </>
-          )}
-          {linkType === 'view' && (
-            <span className="flex items-center gap-x-2">
-              {<FontAwesomeIcon icon={faEye} />}
-              <InlineLink className="font-semibold" file={file} params={params}>
-                {`${t('profile.view')}\u0020`}
-                {linkLabel}
-              </InlineLink>
-            </span>
-          )}
-        </CardFooter>
-      )}
+      {ChildrenWithProps(props)}
     </Card>
+  );
+}
+
+interface ProfileCardContentProps extends ProfileBaseProps {}
+
+export function ProfileCardContent({ children }: ProfileCardContentProps): JSX.Element {
+  return <CardContent className="my-3 space-y-3 p-0">{children}</CardContent>;
+}
+
+interface ProfileCardHeaderProps extends ProfileBaseProps {
+  status?: 'in-progress' | 'complete';
+  required?: boolean;
+  updated?: boolean;
+}
+
+export function ProfileCardHeader({ status, required, updated, children }: ProfileCardHeaderProps): JSX.Element {
+  return (
+    <CardHeader className="p-0">
+      <div className="mb-6 grid justify-between gap-2 select-none sm:grid-cols-2">
+        <div>
+          <CardTitle className="text-2xl">{children}</CardTitle>
+        </div>
+        <div className="space-x-2 sm:ml-auto">
+          {status &&
+            (status === 'complete' ? (
+              <CompleteTag />
+            ) : (
+              <>
+                <InProgressTag />
+                {required && <RequiredTag />}
+              </>
+            ))}
+          {updated && <UpdatedTag />}
+        </div>
+      </div>
+    </CardHeader>
+  );
+}
+
+interface ProfileCardFooterProps extends ProfileExtendedProps {}
+
+export function ProfileCardFooter(props: ProfileCardFooterProps): JSX.Element {
+  const { errorState } = props;
+  return (
+    <CardFooter
+      className={cn(
+        'mt-3',
+        errorState ? 'bg-red-100' : 'bg-gray-100', // Add background
+        '-mx-4 sm:-mx-6', // Pull horizontally to cancel parent padding
+        '-mb-4 sm:-mb-6', // Pull down to cancel parent bottom padding
+        'px-4 sm:px-6', // Add horizontal padding back for the content
+        'py-4', // Add vertical padding for the contents
+        'rounded-b-xs', // Re-apply bottom roundings
+      )}
+    >
+      {ChildrenWithProps(props)}
+    </CardFooter>
+  );
+}
+
+interface ProfileCardEditLinkProps extends ProfileBaseProps {
+  isNew?: boolean;
+  file: I18nRouteFile;
+  params?: Params;
+}
+
+export function ProfileCardEditLink({ isNew, file, params, errorState, children }: ProfileCardEditLinkProps): JSX.Element {
+  const { t } = useTranslation('app');
+  return (
+    <>
+      {errorState && <h3 className="pb-4 text-lg font-bold text-[#333333]">{t('profile.field-incomplete')}</h3>}
+      <span className="flex items-center gap-x-2">
+        {errorState && <FontAwesomeIcon icon={faTriangleExclamation} className="text-red-800" />}
+        {!errorState && (isNew ? <FontAwesomeIcon icon={faPlus} /> : <FontAwesomeIcon icon={faPenToSquare} />)}
+        <InlineLink className={`${errorState && 'text-red-800'} font-semibold`} file={file} params={params}>
+          {`${isNew ? t('profile.add') : t('profile.edit')}\u0020`}
+          {children}
+        </InlineLink>
+      </span>
+    </>
+  );
+}
+
+interface ProfileCardViewLinkProps extends ProfileBaseProps {
+  file: I18nRouteFile;
+  params?: Params;
+}
+
+export function ProfileCardViewLink({ file, params, children }: ProfileCardViewLinkProps): JSX.Element {
+  const { t } = useTranslation('app');
+  return (
+    <span className="flex items-center gap-x-2">
+      {<FontAwesomeIcon icon={faEye} />}
+      <InlineLink className="font-semibold" file={file} params={params}>
+        {`${t('profile.view')}\u0020`}
+        {children}
+      </InlineLink>
+    </span>
   );
 }
 
