cad6bf7f624dd910d007e5ecf4a8a33b13486cca
Faiza Jahanzeb <faiza.jahanzeb@hrsdc-rhdcc.gc.ca>
2025-10-09 08:51:15 -0400

    back to profile link for hr advisor employee profile route

--- frontend/app/routes/hr-advisor/employee-profile/employment-information.tsx
+++ frontend/app/routes/hr-advisor/employee-profile/employment-information.tsx
@@ -125,7 +125,12 @@ export default function EmploymentInformation({ loaderData, actionData, params }
 
   return (
     <>
-      <BackLink aria-label={t('app:profile.back')} className="mt-6" file="routes/employee/profile/index.tsx" params={params}>
+      <BackLink
+        aria-label={t('app:profile.back')}
+        className="mt-6"
+        file="routes/hr-advisor/employee-profile/index.tsx"
+        params={params}
+      >
         {t('app:profile.back')}
       </BackLink>
       <div className="max-w-prose">
