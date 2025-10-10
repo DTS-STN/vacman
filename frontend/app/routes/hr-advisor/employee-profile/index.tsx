417b13a1d60ccd06ef62d84f47aa9448ac177aac
Dario Au <dario.au@hrsdc-rhdcc.gc.ca>
2025-10-10 14:09:40 -0600

    refactor(frontend): refactor background to use background component (#1009)

--- frontend/app/routes/hr-advisor/employee-profile/index.tsx
+++ frontend/app/routes/hr-advisor/employee-profile/index.tsx
@@ -27,6 +27,7 @@ import {
   ProfileCardHeader,
 } from '~/components/profile-card';
 import { ProfileStatusTag } from '~/components/status-tag';
+import { VacmanBackground } from '~/components/vacman-background';
 import { EMPLOYEE_WFA_STATUS, PROFILE_STATUS } from '~/domain/constants';
 import { HttpStatusCodes } from '~/errors/http-status-codes';
 import { useFetcherState } from '~/hooks/use-fetcher-state';
@@ -283,25 +284,19 @@ export default function EditProfile({ loaderData, params }: Route.ComponentProps
 
   return (
     <div className="space-y-8">
-      <div className="absolute left-0 w-full space-y-4 bg-[rgba(9,28,45,1)] py-8 wrap-break-word text-white">
-        <div className="container">
-          {loaderData.profileStatus && (
-            <ProfileStatusTag status={loaderData.profileStatus} lang={loaderData.lang} rounded view="hr-advisor" />
-          )}
-          <PageTitle className="after:w-14" containerClassName="my-6">
-            {loaderData.name}
-          </PageTitle>
-          {loaderData.email && <p className="mt-1">{loaderData.email}</p>}
-          <p className="font-normal text-[#9FA3AD]">
-            {t('app:profile.last-updated', { date: browserTZ, name: loaderData.lastUpdatedBy })}
-          </p>
-        </div>
-        <div
-          role="presentation"
-          className="absolute bottom-0 left-0 h-40 w-full scale-x-[-1] bg-[url('/VacMan-design-element-06.svg')] bg-size-[30rem] bg-left-bottom bg-no-repeat"
-        />
-      </div>
-      <div className="mt-110 justify-between sm:mt-70 md:grid md:grid-cols-2">
+      <VacmanBackground variant="bottom-right">
+        {loaderData.profileStatus && (
+          <ProfileStatusTag status={loaderData.profileStatus} lang={loaderData.lang} rounded view="hr-advisor" />
+        )}
+        <PageTitle className="after:w-14" containerClassName="my-6">
+          {loaderData.name}
+        </PageTitle>
+        {loaderData.email && <p className="mt-1">{loaderData.email}</p>}
+        <p className="font-normal text-[#9FA3AD]">
+          {t('app:profile.last-updated', { date: browserTZ, name: loaderData.lastUpdatedBy })}
+        </p>
+      </VacmanBackground>
+      <div className="justify-between md:grid md:grid-cols-2">
         <div className="max-w-prose">
           <BackLink
             aria-label={t('app:employee-profile.back')}
