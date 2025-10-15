ff20daadf7c34d9a376520c781e43eb6e8247cc6
Marco <marcogoc951@gmail.com>
2025-10-15 13:05:23 -0400

    correcting changeset

--- backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
+++ backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
@@ -24,7 +24,7 @@ DELETE FROM [CD_WORK_SCHEDULE] WHERE [ID] IN (2, 3);
 -- Adding the DIE code to CD_SELECTION_PROCESS_TYPE  AB#7208
 --
 
---changeset system:cd_selection_process_type_adding_DIE_on dbms:mssql,h2
+--changeset system:cd_selection_process_type_adding_DIE_on dbms:mssql
 SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE ON;
 
 --changeset system:cd_selection_process_type_adding_DIE dbms:mssql,h2
@@ -32,5 +32,5 @@ INSERT INTO [CD_SELECTION_PROCESS_TYPE] ([ID], [CODE], [NAME_EN], [NAME_FR], [EF
 VALUES
 (10, 'DIE', 'Deployment - Indeterminate (refer to exceptions)', 'Mutation - Durée indéterminée (veuillez consulter les exceptions)', '1970-01-01 00:00:00', 'system', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP);
 
---changeset system:cd_selection_process_type_adding_DIE_off dbms:mssql,h2
+--changeset system:cd_selection_process_type_adding_DIE_off dbms:mssql
 SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE OFF;
