bdd35757a766fed113a6927a354d82ba72077447
Marco <marcogoc951@gmail.com>
2025-10-15 12:58:05 -0400

    lets see if copilot is correct

--- backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
+++ backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
@@ -25,7 +25,7 @@ DELETE FROM [CD_WORK_SCHEDULE] WHERE [ID] IN (2, 3);
 --
 
 --changeset system:cd_selection_process_type_adding_DIE_on dbms:mssql,h2
---SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE ON;
+SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE ON;
 
 --changeset system:cd_selection_process_type_adding_DIE dbms:mssql,h2
 INSERT INTO [CD_SELECTION_PROCESS_TYPE] ([ID], [CODE], [NAME_EN], [NAME_FR], [EFFECTIVE_DATE], [USER_CREATED], [DATE_CREATED], [USER_UPDATED], [DATE_UPDATED])
@@ -33,4 +33,4 @@ VALUES
 (10, 'DIE', 'Deployment - Indeterminate (refer to exceptions)', 'Mutation - Durée indéterminée (veuillez consulter les exceptions)', '1970-01-01 00:00:00', 'system', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP);
 
 --changeset system:cd_selection_process_type_adding_DIE_off dbms:mssql,h2
---SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE OFF;
+SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE OFF;
