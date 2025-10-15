29f5b18c98397cbfe13721be40e77ea265ffb383
Marco <marcogoc951@gmail.com>
2025-10-15 12:23:51 -0400

    updated sql file

--- backend/src/main/resources/db/changelog/changes/data/db.data.v1.sql
+++ backend/src/main/resources/db/changelog/changes/data/db.data.v1.sql
@@ -976,14 +976,3 @@ VALUES
 
 --changeset system:cd_wfa_status_career_transit_off dbms:mssql
 SET IDENTITY_INSERT CD_WFA_STATUS OFF;
-
---changeset system:cd_selection_process_type_adding_DIE_on dbms:mssql
-SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE ON;
-
---changeset system:cd_selection_process_type_adding_DIE dbms:mssql
-INSERT INTO [CD_SELECTION_PROCESS_TYPE] ([ID], [CODE], [NAME_EN], [NAME_FR], [EFFECTIVE_DATE], [USER_CREATED], [DATE_CREATED], [USER_UPDATED], [DATE_UPDATED])
-VALUES
-(10, 'DIE', 'Deployment - Indeterminate (refer to exceptions)', 'Mutation - Durée indéterminée (veuillez consulter les exceptions)', '1970-01-01 00:00:00', 'system', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP);
-
---changeset system:cd_selection_process_type_adding_DIE_off dbms:mssql
-SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE OFF;
--- backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
+++ backend/src/main/resources/db/changelog/changes/data/db.data.v2.sql
@@ -19,3 +19,18 @@ DELETE FROM [CD_EMPLOYMENT_TENURE] WHERE [ID] IN (2, 3);
 DELETE FROM [CD_WORK_SCHEDULE] WHERE [ID] IN (2, 3);
 
 --------------------------------------------------------------------------------
+
+--
+-- Adding the DIE code to CD_SELECTION_PROCESS_TYPE  AB#7208
+--
+
+--changeset system:cd_selection_process_type_adding_DIE_on dbms:mssql
+SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE ON;
+
+--changeset system:cd_selection_process_type_adding_DIE dbms:mssql
+INSERT INTO [CD_SELECTION_PROCESS_TYPE] ([ID], [CODE], [NAME_EN], [NAME_FR], [EFFECTIVE_DATE], [USER_CREATED], [DATE_CREATED], [USER_UPDATED], [DATE_UPDATED])
+VALUES
+(10, 'DIE', 'Deployment - Indeterminate (refer to exceptions)', 'Mutation - Durée indéterminée (veuillez consulter les exceptions)', '1970-01-01 00:00:00', 'system', CURRENT_TIMESTAMP, 'system', CURRENT_TIMESTAMP);
+
+--changeset system:cd_selection_process_type_adding_DIE_off dbms:mssql
+SET IDENTITY_INSERT CD_SELECTION_PROCESS_TYPE OFF;
