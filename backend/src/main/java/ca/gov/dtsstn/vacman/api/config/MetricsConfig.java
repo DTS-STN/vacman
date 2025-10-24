71863a650e2acc31b769fb526c5b8ea599e057cb
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2025-10-24 06:12:27 -0230

    feat(backend): add MetricsConfig and properties support

---
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/config/MetricsConfig.java
@@ -0,0 +1,33 @@
+package ca.gov.dtsstn.vacman.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ca.gov.dtsstn.vacman.api.config.properties.ApplicationProperties;
+import io.micrometer.core.instrument.binder.MeterBinder;
+
+@Configuration
+public class MetricsConfig {
+
+	@Autowired ApplicationProperties applicationProperties;
+
+	@Autowired GitProperties gitProperties;
+
+	/**
+	 * Configures common tags for all metrics recorded by Micrometer.
+	 * This includes service name, service version, and deployment environment name.
+	 * These tags help in identifying and filtering metrics in monitoring systems.
+	 */
+	@Bean MeterBinder commonTags() {
+		return registry -> {
+			registry.config().commonTags(
+				"service.name", applicationProperties.metrics().serviceName(),
+				"service.version", gitProperties.get("build.version"),
+				"deployment.environment.name", applicationProperties.metrics().environmentName()
+			);
+		};
+	}
+
+}
--- backend/src/main/java/ca/gov/dtsstn/vacman/api/config/properties/ApplicationProperties.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/config/properties/ApplicationProperties.java
@@ -1,6 +1,5 @@
 package ca.gov.dtsstn.vacman.api.config.properties;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
@@ -11,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 @EnableConfigurationProperties({
 	EntraIdProperties.class,
 	GcNotifyProperties.class,
+	MetricsProperties.class,
 	MSGraphProperties.class,
 	RequestMatchingProperties.class,
 	SwaggerUiProperties.class,
@@ -18,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 public record ApplicationProperties(
 	@NestedConfigurationProperty EntraIdProperties entraId,
 	@NestedConfigurationProperty GcNotifyProperties gcnotify,
+	@NestedConfigurationProperty MetricsProperties metrics,
 	@NestedConfigurationProperty RequestMatchingProperties matches,
 	@NestedConfigurationProperty MSGraphProperties msGraph,
 	@NestedConfigurationProperty SwaggerUiProperties swaggerUi
