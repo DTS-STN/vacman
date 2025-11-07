092cba36123db2cfaf89672f0ed96d7a08a4dd3f
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-11-07 10:08:33 -0330

    refactor(backend): remove extraneous fields from `CodeReadModel` (#1186)
    refactor(backend): remove extraneous fields from CodeReadModel

--- backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/CodeReadModel.java
+++ backend/src/main/java/ca/gov/dtsstn/vacman/api/web/model/CodeReadModel.java
@@ -1,7 +1,5 @@
 package ca.gov.dtsstn.vacman.api.web.model;
 
-import java.time.Instant;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import jakarta.annotation.Nullable;
@@ -19,23 +17,5 @@ public interface CodeReadModel {
 
 	@Schema(accessMode = AccessMode.READ_ONLY)
 	@Nullable String getNameFr();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable Instant getEffectiveDate();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable Instant getExpiryDate();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable String getCreatedBy();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable Instant getCreatedDate();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable String getLastModifiedBy();
-
-	@Schema(accessMode = AccessMode.READ_ONLY)
-	@Nullable Instant getLastModifiedDate();
 
 }
